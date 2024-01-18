84149d8c48e80adc1d3505428b2a74fcdf16a133
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-18 16:08:45 +0100

    Updated 05 in 05-ControlStructures

--- groovyTraining-05-ControlStructures/src/05Exercise.groovy
+++ groovyTraining-05-ControlStructures/src/05Exercise.groovy
@@ -1,1 +1,34 @@
+class Account {
+    BigDecimal balance = 0.0
 
+    void deposit(def amount) {
+        if (amount >= 0) {
+            balance += amount
+            println "$amount EUR added to account"
+        } else
+            throw new Exception("Deposit amount cannot be lower than 0 EUR")
+    }
+
+    void deposit(List amounts) {
+        for (amount in amounts) {
+            deposit(amount)
+        }
+    }
+}
+
+Account account = new Account()
+try {
+    account.deposit(100)
+    account.deposit(-50)
+} catch (Exception e) {
+    println e.message
+}
+println account.balance
+
+try {
+    account.deposit([199, -50, 500, -20, 200])
+} catch (Exception e) {
+    println e.message
+}
+println account.balance
+println "---------------------------"
