e468d38711b412c0458272838114015441f9b45b
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-18 12:21:10 +0100

    Updated 03 and added 04 to 04-Closures

--- groovyTraining-04-Closures/src/03ClosureParameters.groovy
+++ groovyTraining-04-Closures/src/03ClosureParameters.groovy
@@ -1,1 +1,47 @@
+// implicit parameter (it)
+def itParam = {
+    println it
+}
+itParam('dan')
 
+// no parameters
+def noParams = { ->
+    println "no params..."
+}
+noParams()
+
+// explicit params (1+)
+def params = { String first, String last ->
+    println "Hello, $first $last"
+}
+params("Dan", "Vega")
+println "---------------------------"
+
+
+// default values
+def defaultValues = { String name, String greeting = "Howdy" ->
+    println "$greeting, $name"
+}
+defaultValues("Dan","Hello")
+defaultValues("Joe")
+println "---------------------------"
+
+
+// var-arg (...): used for accepting an undefined number of arguments (1+). Valid for regular methods too.
+def concat = { String... args ->
+    args.join('.') // the argument of join() is the separator
+}
+println concat('abc','def')
+println concat('abc','def','123','456')
+println "---------------------------"
+
+
+// A Closure object has properties which describe the closure itself (number of params, types, etc.).
+// For example, this can be used to inspect the arguments/parameters.
+def someMethod(Closure c) {
+    println "$c.class: max params $c.maximumNumberOfParameters, param types $c.parameterTypes"
+}
+def someClosure = { int x, int y -> x + y }
+
+someMethod(someClosure)
+println "---------------------------"
