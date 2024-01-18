5e8134d8257c1f0a51aaa0d0579c80c14cb6f7b4
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-18 11:46:24 +0100

    Added 01 and 02 to 04-Closures

---
+++ groovyTraining-04-Closures/src/01WhatAreClosures.groovy
@@ -0,0 +1,20 @@
+/* https://groovy-lang.org/closures.html - https://docs.groovy-lang.org/latest/html/api/groovy/lang/Closure.html
+
+A closure in Groovy is an open, anonymous, block of code that can take arguments, return a value and be assigned to a variable.
+A closure may reference variables declared in its surrounding scope. In opposition to the formal definition of a closure,
+Closure in the Groovy language can also contain free variables which are defined outside of its surrounding scope.
+While breaking the formal concept of a closure, it offers a variety of advantages which are described in this chapter.
+
+Closures are objects (Closure is a class: groovy.lang.Closure<V>) that behave similarly to methods, with the advantages of both structures.
+
+Closures are used for:
+- Iterators
+- Callbacks
+- Higher-order functions
+- Specialized Control Structures
+- Builders
+- Resource allocation
+- Threads
+- DSLs
+- Fluent Interfaces
+*/
