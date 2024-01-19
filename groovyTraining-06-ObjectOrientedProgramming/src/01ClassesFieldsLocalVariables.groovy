7c840f0f16f060c5c019d68ea5eb6c604ec11193
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-19 16:53:57 +0100

    Added 01 and 02 to 06-OOP

---
+++ groovyTraining-06-ObjectOrientedProgramming/src/01ClassesFieldsLocalVariables.groovy
@@ -0,0 +1,23 @@
+// class public by default
+// fields (attributes) private by default
+
+class Person {
+
+    String firstName, lastName
+    def dob
+    protected String f1,f2,f3
+    private Date createdOn = new Date()
+
+    static welcomeMsg = 'HELLO'
+    public static final String WELCOME_MSG = 'HELLO'
+
+    def foo() { //method
+        // local variables: only visible within the method they are declared in
+        String msg = "Hello"
+        String firstName = "Dan"
+        println "$msg, $firstName"
+    }
+}
+
+def person = new Person()
+println person.foo()
---
+++ groovyTraining-06-ObjectOrientedProgramming/src/02ConstructorsAndMethods.groovy
@@ -0,0 +1,44 @@
+import groovy.transform.ToString
+@ToString
+class Person {
+
+    String first,last
+
+    // constructor (it's a special method)
+    Person(String fullName) {
+        List nameParts = fullName.split(' ')
+        first = nameParts[0]
+        last = nameParts[1]
+    }
+
+    // methods
+    public void foo(String a, String b) {
+        // do stuff
+    }
+
+    String getFullName(){
+        "$first $last"
+    }
+
+    def bar(){
+
+    }
+
+    static String doGoodWork(){
+        println "doing good work..."
+    }
+
+    List someMethod(List numbers = [1,2,3], Boolean canAccessAll = false){
+
+    }
+
+    def concat(String... args) {
+        println args.length
+    }
+
+}
+
+Person p = new Person("Dan Vega")
+println p.getFullName()
+
+p.concat('a','b','c','d')
