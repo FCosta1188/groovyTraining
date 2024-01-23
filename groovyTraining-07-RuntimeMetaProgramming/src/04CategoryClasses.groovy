f409ef6c057cfd74d8c108095c985ef94bb71480
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-23 14:47:21 +0100

    Added 04, 05 in 07

--- groovyTraining-07-RuntimeMetaProgramming/src/04CategoryClasses.groovy
+++ groovyTraining-07-RuntimeMetaProgramming/src/04CategoryClasses.groovy
@@ -1,1 +1,26 @@
+import groovy.time.TimeCategory
 
+
+// custom category class: it contains static methods which expand the original String class
+class StringCategory {
+    static String shout(String str){
+        str.toUpperCase()
+    }
+}
+
+use(StringCategory) { // the use keyword is needed to define the code block where the category class will be available
+    println "hello".shout()
+}
+println"------------------------------"
+
+// built-in Groovy class
+use(TimeCategory) {
+
+    println 1.minute.from.now
+    println 10.hours.ago
+
+    def someDate = new Date()
+    println someDate - 3.months
+
+}
+println"------------------------------"
