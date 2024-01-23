9e0d560533ece34f6d726389e208a196b44c860e
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-23 12:44:11 +0100

    Updated 03 and added 04 in 07

--- groovyTraining-07-RuntimeMetaProgramming/src/03MetaClass.groovy
+++ groovyTraining-07-RuntimeMetaProgramming/src/03MetaClass.groovy
@@ -1,21 +1,42 @@
-// MetaClass Demo
+/* https://docs.groovy-lang.org/latest/html/api/groovy/lang/MetaClass.html
+A MetaClass (interface) within Groovy defines the behaviour of any given Groovy or Java class.
+Each object has a property metaClass of type MetaClass, which is dynamically expandable.
+In this way, even custom regular classes can be expanded by using the features of their metaClass property.
+*/
+
+//Expando (groovy.util): represents a dynamically expandable bean, like MetaClass.
+Expando e1 = new Expando()
+e1.name = 'Dan' // new property, not defined withing the class definition, but defined dynamically at runtime
+e1.expandCode = { -> println "$name loves to expand code..."} // new method (defined through a closure), not defined withing the class definition, but defined dynamically at runtime
+e1.expandCode()
+
+Expando e2 = new Expando()
+// e2.expandCode() // does not work, because dynamic properties and methods are instance-based
+println "-----------------------------------"
 
 
-//Expando e = new Expando()
-//e.name = 'Dan'
-//e.writeCode = { -> println "$name loves to write code..."}
-//e.writeCode()
-
+// To expand custom classes like seen in Expando, we can use their metaClass property.
 class Developer {
+    String name
 
+    Developer(String name) {
+        this.name = name
+    }
 }
-// per instance
+
+// per instance: when the new property/method is defined on an object
 Developer dan = new Developer()
 dan.metaClass.name = 'Dan'
 dan.metaClass.writeCode = { -> println "$name loves to write code..."}
 dan.writeCode()
 
-// every instance
+// every instance: when the new property/method is defined on a class (both custom and built-in Groovy and Java classes)
+Developer.metaClass.writeDocs = { -> println "$name loves to write documentation..." }
+Developer d1 = new Developer("Pinco")
+Developer d2 = new Developer("Tizio")
+d1.writeDocs()
+d2.writeDocs()
+
 String.metaClass.shout = { -> toUpperCase() }
 println "hello dan".shout()
-println ""
+println "-----------------------------------"
