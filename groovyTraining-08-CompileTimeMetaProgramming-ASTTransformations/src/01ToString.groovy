2725dac531cca9c76ac81dc10f505c054b2d93c5
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-23 17:19:46 +0100

    Added 01-10 to 08-CompileTimeMetaProgramming

--- groovyTraining-08-CompileTimeMetaProgramming-ASTTransformations/src/01ToString.groovy
+++ groovyTraining-08-CompileTimeMetaProgramming-ASTTransformations/src/01ToString.groovy
@@ -1,7 +1,7 @@
 //https://docs.groovy-lang.org/next/html/gapi/groovy/transform/ToString.html
 import groovy.transform.ToString
 
-@ToString(includeNames = true, excludes = ["email"])
+@ToString(includeNames = true, excludes = ["email"]) // see docs for other options
 class Person {
     String first
     String last
@@ -9,6 +9,5 @@ class Person {
 }
 
 Person p = new Person(first:"Dan",last: "Vega", email: "danvega@gmail.com")
-
 println p.toString()
 println"--------------------------"
--- groovyTraining-08-CompileTimeMetaProgramming-ASTTransformations/src/03TupleContructor.groovy
+++ groovyTraining-08-CompileTimeMetaProgramming-ASTTransformations/src/03TupleContructor.groovy
@@ -1,1 +1,15 @@
 //https://docs.groovy-lang.org/next/html/gapi/groovy/transform/TupleConstructor.html
+import groovy.transform.ToString
+import groovy.transform.TupleConstructor
+
+@ToString
+@TupleConstructor
+class Person {
+    String first
+    String last
+    String email
+}
+
+Person p = new Person("Dan","Vega","danvega@gmail.com")
+println p.toString()
+println"--------------------------"
