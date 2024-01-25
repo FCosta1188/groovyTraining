c745d04e17dc20aceac670ebbc76bb6979371668
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-25 13:05:58 +0100

    Added 01-03 to 09-WorkingWithBuilders

--- groovyTraining-09-WorkingWithBuilders/src/01MarkupBuilder-XML.groovy
+++ groovyTraining-09-WorkingWithBuilders/src/01MarkupBuilder-XML.groovy
@@ -1,1 +1,25 @@
+//https://docs.groovy-lang.org/latest/html/api/groovy/xml/MarkupBuilder.html
+import groovy.xml.MarkupBuilder
 
+MarkupBuilder builder = new MarkupBuilder()
+builder.omitEmptyAttributes = true
+builder.omitNullAttributes = true
+
+// by default, it will be printed on the console when running the script (no need for println). Other options available (file, stream, etc.)
+builder.sports { // sport and name are tags (outer and inner), id and foo are attributes
+    sport(id:1) {
+        name 'Baseball'
+    }
+    sport(id:2) {
+        name 'Basketball'
+    }
+    sport(id:3) {
+        name 'Football'
+    }
+    sport(id:4) {
+        name 'Hockey'
+    }
+    sport(id:null, foo:'') { // omitted as per options above
+        name '' // not omitted
+    }
+}
--- groovyTraining-09-WorkingWithBuilders/src/Main.groovy
+++ groovyTraining-09-WorkingWithBuilders/src/Main.groovy
@@ -1,3 +1,5 @@
+// for builder usage examples, see Groovy source code on GIT (specifically, unit tests): https://github.com/apache/groovy.
+
 static void main(String[] args) {
   println "Hello world!"
 }
