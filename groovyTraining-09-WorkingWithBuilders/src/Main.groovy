ed68dae9052c04e4ea322cb5fc3f9a2843451e27
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-26 13:02:20 +0100

    Updated 04-05 in 09-WorkingWithBuilders

--- groovyTraining-09-WorkingWithBuilders/src/Main.groovy
+++ groovyTraining-09-WorkingWithBuilders/src/Main.groovy
@@ -1,5 +1,6 @@
+// list of Groovy builders: https://groovy-lang.org/single-page-documentation.html
 // for builder usage examples, see Groovy source code on GIT (specifically, unit tests): https://github.com/apache/groovy.
 
 static void main(String[] args) {
-  println "Hello world!"
+  println "Hello builders!"
 }
