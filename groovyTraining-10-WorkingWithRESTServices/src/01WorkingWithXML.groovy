c3227d556429fbc782254a193798f547e1dfdad5
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-26 13:17:54 +0100

    Added 01,02 in 10-WorkingWithRESTServices

---
+++ groovyTraining-10-WorkingWithRESTServices/src/01WorkingWithXML.groovy
@@ -0,0 +1,42 @@
+import groovy.xml.MarkupBuilder
+import groovy.xml.XmlSlurper
+
+FileWriter writer = new FileWriter('data/sports.xml')
+MarkupBuilder builder = new MarkupBuilder(writer)
+builder.doubleQuotes = true
+
+builder.sports {
+    sport(id:1) {
+        name 'Baseball'
+    }
+    sport(id:2) {
+        name 'Basketball'
+    }
+    sport(id:3) {
+        name 'Football'
+    }
+    sport(id:4) {
+        name 'Hockey'
+    }
+
+}
+
+def sports = new XmlSlurper().parse('data/sports.xml')
+println sports
+println sports.sport[2].name
+println '----------------------------'
+
+/*
+def xml = '''
+    <sports>
+        <sport>
+            <name>Football</name>
+        </sport>
+    </sports>
+'''
+
+def sports = new XmlSlurper().parseText(xml)
+
+println sports.getClass().getName()
+println sports.sport.name
+*/
