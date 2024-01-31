3dabd30ee43e74dd8e9b7c475a928734416464ff
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-01-31 08:52:04 +0100

    Updated 01-02 in 11-Bonus

--- groovyTraining-12-Bonus/src/02GroovyAndSpringBoot.groovy
+++ groovyTraining-12-Bonus/src/02GroovyAndSpringBoot.groovy
@@ -1,16 +1,3 @@
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
-
-@RunWith(SpringRunner)
-@SpringBootTest
-class HelloGroovyApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-}
-
 @RestController
 class GreetingController {
     @RequestMapping("/{name}")
